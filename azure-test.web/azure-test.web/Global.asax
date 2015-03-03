b81a55e093947e5ca1e0d8fd71882903cff214ed
James High <jhigh2000@gmail.com>
2015-03-03 16:25:43 -0600

    Initial Load

---
+++ azure-test.web/azure-test.web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="azure_test.web.MvcApplication" Language="C#" %>
---
+++ azure-test.web/azure-test.web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(azure_test.web.Startup))]
+namespace azure_test.web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
